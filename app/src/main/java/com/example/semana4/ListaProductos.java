d98e3e476a36ddafe8ff13bae9f132e1fdcb4d8b
emilymirand <125322859+emilymirand@users.noreply.github.com>
2023-07-01 21:59:05 -0500

    nuevo AGREGAR

---
+++ app/src/main/java/com/example/semana4/ListaProductos.java
@@ -0,0 +1,79 @@
+package com.example.semana4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ListaProductos extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_bancos);
+
+
+        Bundle bundle = getIntent().getExtras();
+        Map<String, String> datos = new HashMap<>();
+        datos.put("fuente", "1");
+
+        String url = "https://api.uealecpeterson.net/public/productos/search";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(datos),
+                this, this) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + bundle.getString("TOKEN"));
+                return headers;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        TextView Informacion = (TextView)findViewById(R.id.informacion);
+        Informacion.setText("ERROR");
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+
+        ArrayList<String> lstBancos = new ArrayList<String>();
+        JSONArray productosArray = null;
+        try {
+
+            productosArray = response.getJSONArray("productos");
+
+            for (int i = 0; i < productosArray.length(); i++) {
+                JSONObject producto = productosArray.getJSONObject(i);
+                lstBancos.add("("+i+")"+producto.getString("barcode").toString()+", "+" "+" "+producto.getString("descripcion").toString()
+                        +producto.getString("costo").toString()+", "+" "+" "+producto.getString("impuesto").toString()+"\n");
+            }
+
+            TextView txtvolley = findViewById(R.id.informacion);
+            txtvolley.setText(lstBancos.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/semana4/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.semana4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import WebService.Asynchtask;
+import WebService.WebService;
+
+public class MainActivity extends AppCompatActivity implements Asynchtask {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void BtIngresar (View view){
+        EditText Nombre = findViewById(R.id.nombre);
+        EditText Contraseña = findViewById(R.id.contraseña);
+        String nombre;
+        String  contraseña;
+        nombre=Nombre.getText().toString();
+        contraseña=Contraseña.getText().toString();
+        //codigo para concetar a internet
+        Bundle bundle = this.getIntent().getExtras();
+        Map<String, String> datos = new HashMap<String, String>();
+        datos.put("correo",nombre);
+        datos.put("clave",contraseña);
+        WebService ws= new WebService(" https://api.uealecpeterson.net/public/login"
+                ,datos, MainActivity.this, MainActivity.this);
+        ws.execute("POST");
+
+    }
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+        TextView respuesta =findViewById(R.id.respuesta);
+        JSONObject jsonrespuesta = new JSONObject(result);
+
+        if (jsonrespuesta.has("error"))
+        {
+            respuesta.setText(jsonrespuesta.getString("error"));
+        }
+        else
+        {
+
+            Bundle b = new Bundle();
+            b.putString("TOKEN", jsonrespuesta.getString("access_token"));
+            Intent intent = new Intent(MainActivity.this, ListaProductos.class);
+            intent.putExtras(b);
+            startActivity(intent);
+
+        }
+
+
+    }
+}
